4ceace9b5e021618e16db6053cc91730954b816e
Nick Kneupper <nkneupper117@gmail.com>
2023-04-06 11:20:39 -0400

    added .env for additional key security

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 username = 'YOUR_REDDIT_NAME'
 password = 'YOUR_REDDIT_PW'
+
+google_api_key = os.getenv('GOOGLE_API_KEY')
 
 client_id = 'YOUR_REDDIT_APP_ID'
 client_secret = 'YOUR_TOP_SECRET_REDDIT_APP_CODE'
